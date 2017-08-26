022fd469309bb0577619ae9d4efa3b9488d06343
Eric Olszewski <eolszewski@gmail.com>
2017-08-25 23:01:20 -0500

    Working...

--- contracts/Youtube.sol
+++ contracts/Youtube.sol
@@ -1,25 +1,24 @@
 pragma solidity ^0.4.4;
 
-import "./oraclizeAPI.sol";
+import "./usingOraclize.sol";
 
 contract Youtube is usingOraclize {
 
     string public viewsCount;
 
+    event ViewsReceived(string views);
+
     function Youtube() {
-        //OAR = OraclizeAddrResolverI(resolve_addr);  //add this line if you are using Oraclize in private chain environment
-        update(0);
+        OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
     }
 
     function __callback(bytes32 myid, string result) {
         if (msg.sender != oraclize_cbAddress()) throw;
         viewsCount = result;
+        ViewsReceived(result);
     }
 
     function update(uint delay) {
         oraclize_query(delay, 'URL', 'html(https://www.youtube.com/watch?v=9bZkp7q19f0).xpath(//*[contains(@class, "watch-view-count")]/text())');
-    }
-    function getViews() returns(string) {
-        return viewsCount;
     }
 }
